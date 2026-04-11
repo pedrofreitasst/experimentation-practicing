3a9b19f1c2c057978f673c539f97a2cfde7bad89
pedrofreitasst <pedrofreitasst@gmail.com>
2026-04-11 15:48:17 -0300

    Atualizando agora com a aula e práticas relacionadas a control-flow

--- javascript-basico/control-flow.js
+++ javascript-basico/control-flow.js
@@ -61,11 +61,86 @@ const nomes2=['pedro', 'clara', 'vincenzo'];
 
 if (nomes2.length>2){ console.log('uma familia linda');} //se a condição for verdadeira, ele executa o código dentro do bloco
 
-const senha = '123456';
+/*const senha = '123456';
 
 if (senha.length >= 8){ console.log('senha forte o suficiente');}
 
-else {console.log('senha pequena demais');} //se a condição for falsa, ele executa o código dentro do bloco else
+    else if(senha.length >= 6 && senha.length < 8){console.log('quaaase lá, mas ainda não é o suficiente');} //para quando o valor está entre valores, usando else if para criar uma nova condição em um efeito quase cascata
+    
+    
+        else{console.log('senha pequena demais');} //se a condição for falsa, ele executa o código dentro do bloco else
+ 
+*/
+
+//operadores OU || e E &&
+
+const senha = '1234#s56';
+
+if (senha.length >= 8 && senha.includes('#')){ console.log('senha forte o suficiente');}
+
+    else if(senha.length >= 6 || senha.length <8 && senha.includes('#')){console.log('Falta alguns caracteres');} //para quando o valor está entre valores, usando else if para criar uma nova condição em um efeito quase cascata
+    
+    
+        else{console.log('senha pequena demais');} //se a condição for falsa, ele executa o código dentro do bloco else
+
+//para executar se a condição for falsa:
+
+let usuario =false;
+
+if (!usuario){ console.log('usuario não logado');} //o ! inverte o valor da variável, ou seja, se usuario for false, ele vai executar o código dentro do bloco, se usuario for true, ele não vai executar o código dentro do bloco
 
 
+//break e continue
+
+const numeros = [1,2,3,4,5,6,17,18,920];
+
+for(let i = 0; i<numeros.length; i++){
+
+
+    if(numeros[i] === 1){continue;}//quando o numero 1 for encontrado, o loop vai pular para a próxima iteração, ou seja, ele não vai executar o código dentro do bloco para o numero 1, mas vai continuar executando para os outros numeros
+
+    console.log(numeros[i]);
+
+
+
+    if(numeros[i] === 7){console.log('recebeu break');   break;} //quando o numero 7 for encontrado, o loop vai ser interrompido e não vai mais executar o código dentro do bloco
+
+   
+
+
+}
+
+
+//switch
+
+const nota = '9';
+
+switch (nota) {
+
+case '10':
+    console.log('nota maxima');
+    break;
+
+case '9':
+    console.log('nota quase maxima');
+    break;
+
+default:
+    console.log('nota invalida');
+
+
+}
+
+//variaveis e escopo
+
+let idade2 = 30; //variavel global, ou seja, pode ser acessada de qualquer lugar do código
+
+if(true){
+    let idade2 = 31; //variavel local, só afeta dentro do próprio bloco de codigo, ou seja, dentro do if, fora do if a variavel idade2 continua sendo 30
+//qualquer variavel criada dentro de um blco de codigo, como um if, um for, um while, etc, é uma variavel local, ou seja, só pode ser acessada dentro do próprio bloco de codigo, fora do bloco de codigo ela não existe, então não retorna nada. nem undefined.
+    console.log('dentro do bloco de codigo', idade2); //variavel global, ou seja, pode ser acessada de qualquer lugar do código
+ if(true){console.log('dentro de um segundo bloco de codigo', idade2);} //sendo um bloco dentro do outro, a variavel idade2 retorna o valor mais próximo, vulgo o modificado por ultimo, ou 31, e se eu modificar o valor com let ou const dentro desse segundo bloco, ele tera o escopo dentro dele mesmo
+}
+
+console.log('fora do bloco de codigo', idade2); //variavel global, ou seja, pode ser acessada de qualquer lugar do código
 
